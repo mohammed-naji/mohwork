b26e6f91359cbdcb3909fb43917c9a2c2384bd5f
Mohammed Naji <m.naji1512@gmail.com>
2023-08-06 15:39:28 +0300

    all tables

--- database/migrations/2023_08_03_133517_create_admins_table.php
+++ database/migrations/2023_08_03_133517_create_admins_table.php
@@ -11,18 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('phone')->nullable();
-            $table->string('image')->nullable();
-            $table->text('address')->nullable();
-            $table->boolean('is_active')->default(0);
-            $table->enum('type', ['client', 'freelancer'])->default('freelancer');
             $table->rememberToken();
+            $table->foreignId('role_id');
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('admins');
     }
 };
---
+++ database/migrations/2023_08_06_121326_create_reviews_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('job_id');
+            $table->integer('quality');
+            $table->integer('skill');
+            $table->integer('communication');
+            $table->integer('availability');
+            $table->integer('deadline');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
